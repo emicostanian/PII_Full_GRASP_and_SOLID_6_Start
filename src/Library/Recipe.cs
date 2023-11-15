f0ac6b93ce899c44d5df5f5b8bfea17d7f1dd3be
emicostanian <mariaemiliacostanian@gmail.com>
2023-11-15 17:55:43 -0300

    listo

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -6,24 +6,24 @@
 
 using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Full_GRASP_And_SOLID
 {
-    public class Recipe : IRecipeContent // Modificado por DIP
+    public class Recipe : IRecipeContent
     {
-        // Cambiado por OCP
         private IList<BaseStep> steps = new List<BaseStep>();
 
         public Product FinalProduct { get; set; }
 
-        // Agregado por Creator
+        public bool Cooked { get; private set; } = false;
+
         public void AddStep(Product input, double quantity, Equipment equipment, int time)
         {
             Step step = new Step(input, quantity, equipment, time);
             this.steps.Add(step);
         }
 
-        // Agregado por OCP y Creator
         public void AddStep(string description, int time)
         {
             WaitStep step = new WaitStep(description, time);
@@ -35,32 +35,77 @@ namespace Full_GRASP_And_SOLID
             this.steps.Remove(step);
         }
 
-        // Agregado por SRP
         public string GetTextToPrint()
         {
-            string result = $"Receta de {this.FinalProduct.Description}:\n";
-            foreach (BaseStep step in this.steps)
+            if (this.FinalProduct == null)
             {
-                result = result + step.GetTextToPrint() + "\n";
+                throw new InvalidOperationException("FinalProduct cannot be null.");
             }
 
-            // Agregado por Expert
-            result = result + $"Costo de producción: {this.GetProductionCost()}";
+            StringBuilder result = new StringBuilder($"Receta de {this.FinalProduct.Description}:\n");
 
-            return result;
+            foreach (BaseStep step in this.steps)
+            {
+                result.AppendLine(step.GetTextToPrint());
+            }
+
+            result.AppendLine($"Costo de producción: {this.GetProductionCost()}");
+            result.AppendLine($"Tiempo total de cocción: {this.GetCookTime()} minutos");
+            result.AppendLine($"¿Cocido? {this.Cooked}");
+
+            return result.ToString();
         }
 
-        // Agregado por Expert
         public double GetProductionCost()
         {
             double result = 0;
 
             foreach (BaseStep step in this.steps)
             {
-                result = result + step.GetStepCost();
+                result += step.GetStepCost();
             }
 
             return result;
         }
+
+        public int GetCookTime()
+        {
+            int totalCookTime = 0;
+
+            foreach (BaseStep step in this.steps)
+            {
+                if (step is Step)
+                {
+                    totalCookTime += ((Step)step).Time; 
+                }
+            }
+
+            return totalCookTime;
+        }
+
+        public void Cook()
+        {
+            int cookTime = GetCookTime();
+
+            Cooked = true;
+        }
+    
+
+    public class RecipeAdapter : TimerClient
+    {
+        private Recipe recipe;
+        public RecipeAdapter(Recipe recipe)
+        {
+            this.recipe = recipe;
+        }
+        public void TimeOut()
+        {
+            this.recipe.Cooked=true;
+        }
     }
+
 }
+}
+
+// Que patrón(es) o principio(s) has usado para esto?
+// SRP, DIP E ISP, GRASP y SOLID.
